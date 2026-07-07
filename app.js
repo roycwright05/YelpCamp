6177f43146a3285ba070f30ba58d533d056af79b
mrwright05 <mr_wright_05@yahoo.com>
2026-07-07 19:29:39 -0400

    This is the ejs setup completed.

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const port = 6190
 const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
 
     res.send(`<h1>Yes, you are listening on port ${port}</h1>`)
+})
+
+app.get('/home', (req, res) => {
+
+    res.render('home')
 })
 
 app.listen(port, () => 
