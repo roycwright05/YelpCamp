72734468131ebf80d039d5558f4744ab27e83ee5
mrwright05 <mr_wright_05@yahoo.com>
2026-07-07 20:14:44 -0400

    Set up the database in the app file.

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const { errorMonitor } = require('events')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp').then(() => {
+    console.log(`MONGO CONNECTION OPEN`)
+})
+.catch( err => {
+    console.log(`HOUSTON. WE HAVE A PROBLEM`)
+    console.log(err)
+})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+    console.log('Database connected')
+})
+
 const port = 6190
 const app = express()
-const path = require('path')
+
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+
 app.get('/', (req, res) => {
-
-    res.send(`<h1>Yes, you are listening on port ${port}</h1>`)
-})
-
-app.get('/home', (req, res) => {
 
     res.render('home')
 })
