5eb7ceee88712ed9d70e2f48232e657cdc4d4cdd
mrwright05 <mr_wright_05@yahoo.com>
2026-07-21 19:20:47 -0400

    Add images and a description and re-seed the database.

--- seeds/index.js
+++ seeds/index.js
@@ -26,10 +26,13 @@ const seedDB = async () => {
     await Campground.deleteMany({})
     for (let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000) + 1
-
+        const price = Math.floor(Math.random() * 20 ) + 10
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum adipisci suscipit, esse ut id nisi, soluta, officiis quas impedit nulla accusantium consequatur dolorum laudantium. Doloremque enim dolor atque nulla necessitatibus.',
+            price
         })
         await camp.save()
     }
